07d9b0032461b70531e355101248e4c07f39346d
YeshuaGonzalez <yesheli241200@gmail.com>
2023-05-01 12:32:49 -0600

    se agrego un combo box y lista de historial de canales

--- gui_probe.py
+++ gui_probe.py
@@ -13,21 +13,8 @@ import numpy as np
 #import matplotlib.pyplot as plt
 #from tqdm import tqdm
 
-
-#Charging phase 
-# path_file = "E:\\ExpRecientes\\PRK20220816s01_RAW.h5"
-# file = h5py.File(path_file)
-# channel_data = file['Data']['Recording_0']['AnalogStream']['Stream_0']['ChannelData']
-# signal_data = np.array(channel_data[52])
-
-# x_data=[]
-# y_data=[]
-# creating data
-# for i in range(0, 1000):
-#     x_data.append(i)
-#     y_data.append(signal_data[i])
-    
-
+#global variables
+channel_list = []
 
 # Create GUI
 dpg.create_context()
@@ -41,6 +28,26 @@ def callback(sender, app_data):
     print("App Data: ", app_data)
     print("file_path: {}".format(app_data['file_path_name']))
     dpg.set_value(label1_control,app_data['file_path_name'])
+    
+    print("Charging file ... ")
+    
+    path_file = app_data['file_path_name']
+    #print(os.path.abspath(path_file))
+    try :
+        global file 
+        file = h5py.File(path_file)
+        global channel_data
+        channel_data = file['Data']['Recording_0']['AnalogStream']['Stream_0']['ChannelData']
+        print("Point")
+        options = list(range(0,len(channel_data)))
+        dpg.configure_item(combo1, items = options)
+        
+        
+    except: 
+        print("Error to charge file")
+    
+    print("End charge ... ")   
+    
 
 def cancel_callback(sender, app_data):
     print('Cancel was clicked.')
@@ -49,30 +56,33 @@ def cancel_callback(sender, app_data):
     
 def callback_plot(sender, app_data):
     
-    if dpg.get_value(label1_control) != "File path" :
-        print("Charging file ... ")
-        path_file = dpg.get_value(label1_control)
-        #print(os.path.abspath(path_file))
-        try :
-            file = h5py.File(path_file)
-        except: 
-            print("Error to charge file")
+    print('file' in globals())
+    print('channel_data' in globals())
+    if dpg.get_value(label1_control) != "File path" and 'file' in globals() and 'channel_data' in globals():
         
-        channel_data = file['Data']['Recording_0']['AnalogStream']['Stream_0']['ChannelData']
-        signal_data = np.array(channel_data[52])
+        index = dpg.get_value(combo1)
         
-        x_data_np = np.arange(0, 10000, 1)
-        y_data_np = signal_data[0:10000]
-        print("Finish...")
-        #series belong to a y axis
-        dpg.add_line_series(x_data_np, y_data_np, label="ch 52", parent="y_axis")
+        if index != "Channel" and not(int(index) in channel_list):
+            signal_data = np.array(channel_data[int(index)])
+            
+            x_data_np = np.arange(0, 10000, 1)
+            y_data_np = signal_data[0:10000]
+            
+            #series belong to a y axis
+            dpg.add_line_series(x_data_np, y_data_np, label="ch {}".format(index), parent="y_axis")
+            channel_list.append(int(index))
+            print("Finish...")
+            print(channel_list)
+        else :
+            print("Este canal ya se cargo / Seleccione un canal")
+            print(channel_list)
         
     else:
         print("Callback plot ready!")
     
 #__________________________________________________
 
-dpg.create_viewport(title='Custom Title', width=1000, height=700)
+dpg.create_viewport(title='GUI test', width=1000, height=700)
 
 # file & directory selector configuration
 with dpg.file_dialog(directory_selector = False,
@@ -81,7 +91,8 @@ with dpg.file_dialog(directory_selector = False,
                       tag = "file_dialog_id",
                       cancel_callback = cancel_callback,
                       width = 700,
-                      height= 400):
+                      height= 400,
+                      default_path = "E:\ExpRecientes"):
     dpg.add_file_extension(".h5", color = (255, 0, 255, 255), custom_text = "[H5]")
     dpg.add_file_extension(".*")
     
@@ -98,8 +109,13 @@ with dpg.window(label="Simple plot example"):
     # label: Path File Name
     label1_control = dpg.add_text("File path")
     
-    dpg.add_button(label = "Plot segment 52",
+    dpg.add_button(label = "Plot channel",
                    callback = callback_plot)
+    dpg.add_same_line()
+    
+    combo1 = dpg.add_combo(("Channel"), default_value = "Channel")
+    
+    #dpg.configure_item(combo1, items = ("Opción 3", "Opción 4", " "))
 
     #create plot
     with dpg.plot(label="Raw Data", width=800, height=600):
